153f55436aaa356bcb601a7a3c29418d8e0a3058
zzj <1067060242@qq.com>
2015-08-17 09:25:09 +0800

    zzj

---
+++ src/osprey_adphone_hn/cellcom/com/cn/activity/dhb/DhbSyzxBsFragment.java
@@ -0,0 +1,27 @@
+package osprey_adphone_hn.cellcom.com.cn.activity.dhb;
+
+import osprey_adphone_hn.cellcom.com.cn.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class DhbSyzxBsFragment extends Fragment {
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View inflate = inflater.inflate(R.layout.os_dhb_syzx_bs_fragment,
+				container, false);
+		GridView gv = (GridView) inflate
+				.findViewById(R.id.gv_bs_os_dhb_syzx_fragment);
+
+		return inflate;// 加载期间，展开的layout是否应当附着到ViewGroup上
+	}
+}
